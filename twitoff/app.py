e00ccfc623c1b98b17f80aa865051df83465fc27
AVData <AVData@users.noreply.github.com>
2020-01-27 14:46:42 -0800

    init app

---
+++ twitoff/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return 'The Index Page'
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World'
+
+    @app.route('/user/<username>')
+    def show_user_profile(username):
+        # show the user profile for that user
+        return f'{username}'
+    return app
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
