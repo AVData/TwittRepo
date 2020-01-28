ec408aa260f573a5a9097cb8851609e2c73cf863
AVData <AVData@users.noreply.github.com>
2020-01-28 10:35:26 -0800

    modified Pipfiles and including new files

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,11 +1,18 @@
+import uuid
 from flask import Flask
-
+from .models import DB, User, Tweet
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_db.sqlite'
+    DB.init_app(app)
 
     @app.route('/')
     def index():
+        rand_name = str(uuid.uuid4())
+        rand_u = User(name=rand_name)
+        DB.session.add(rand_u)
+        DB.session.commit()
         return 'The Index Page'
 
     @app.route('/hello')
@@ -18,5 +25,5 @@ def create_app():
         return f'{username}'
     return app
 
-if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+# if __name__ == "__main__":
+#     app.run(debug=True, port=8080)
