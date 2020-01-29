8d6fa7b3ff1eadc42694815c58a2c8dfdfc1fa9a
AVData <AVData@users.noreply.github.com>
2020-01-28 17:23:24 -0800

    imported the following libraries tweepy, basilica, python dotenv, and python decouple

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,29 +1,29 @@
-import uuid
-from flask import Flask
-from .models import DB, User, Tweet
+from decouple import config
+from dotenv import load_dotenv
+from flask import Flask, render_template
+from .models import DB, User
+
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_db.sqlite'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
     DB.init_app(app)
 
     @app.route('/')
-    def index():
-        rand_name = str(uuid.uuid4())
-        rand_u = User(name=rand_name)
-        DB.session.add(rand_u)
-        DB.session.commit()
-        return 'The Index Page'
+    def root():
+        return render_template('base.html', title='the Space Jam!', users=User.query.all())
 
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World'
-
-    @app.route('/user/<username>')
-    def show_user_profile(username):
-        # show the user profile for that user
-        return f'{username}'
     return app
-
+#     @app.route('/welcome')
+#     def root():
+#         return render_template('base.html', title='the Space Jam!')
+#
+#     @app.route('/user/<username>')
+#     def show_user_profile(username):
+#         # show the user profile for that user
+#         return f'{username}'
+#     return app
+#
 # if __name__ == "__main__":
 #     app.run(debug=True, port=8080)
