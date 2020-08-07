b5f5c2dbd4b57be0d93c557965256ad7d232aeb3
AVData <AVData@users.noreply.github.com>
2020-08-06 20:46:03 -0700

    Comment on running locally

--- twitoff/__init__.py
+++ twitoff/__init__.py
@@ -1,6 +1,6 @@
 # Entry point for twitoff application
 # in order to run application you must type the .py file in the terminal
-# as such: FLASK_APP=file_name:APP flask run
+# as such: FLASK_APP=twitoff:APP flask run
 
 from .app import create_app
 
